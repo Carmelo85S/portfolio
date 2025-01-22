ce5fea333ba9f293c550c26bfad648d614f42e01
Carmelo85S <carmelo.salis85@gmail.com>
2025-01-22 15:36:53 +0100

    setting per deploy

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -23,7 +23,7 @@ const About = () => {
                 I am always been a creative person, in this moment of life i like to create beautifull webpage from scratch or Figma Design.
                 After attending few course in Java i figured out that i wanted to to something more "visual", so i started approaching front end development 
                 through on line resource and soon i was able to create and style webpage.
-                Taking advanced course followed by a mento and being part of a community of developer, helped me to discover and learn more advance technology 
+                Taking advanced course followed by a mentor and being part of a community of developer, helped me to discover and learn more advanced technology and tools
                 than just HTML. I like to write my website in <span>React</span> and style with <span>Sass</span>. When creating a new web application 
                 i take into consideration the user experience. I combine good design, technology and innovation in all my project.
                 Currently i am focused in learning and undestand more Back end development.
